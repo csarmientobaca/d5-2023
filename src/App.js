92049a0f7af89009bd58ed687cb579e8297b3e87
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-10 17:36:30 +0100

    added props from app to home

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import DropDown from './components/DropDown';
 import Footer from './components/Footer';
 import NavTop from './components/Nav';
-import TVShowsHeader from './components/TvShowHeader';
 import { Container } from 'react-bootstrap';
+import Home from './components/Home';
 
 function App() {
+
+
+
+
+
+
+
   return (
     <>
       <NavTop />
       <Container className="px-4">
 
         <DropDown />
-        <TVShowsHeader />
+        <Home title="harry" />
 
 
       </Container>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,14 +8,15 @@ import f from "../assets/f.png"
 
 
 
-const Carousel = () => {
+const Carousel = (props) => {
 
 
     return (
 
         <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center">
             <Col className="mb-2 px-1">
-                <img className="img-fluid" src={a} alt="movie picture" />
+                {/* <img className="img-fluid" src={props.poster} alt="movie picture" /> */}
+                <p>{props.poster}</p>
             </Col>
             <Col className="mb-2 px-1">
                 <img className="img-fluid" src={b} alt="movie picture" />
